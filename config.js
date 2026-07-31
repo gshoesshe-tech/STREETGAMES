f3ce7a011d5eec681a80b439451173ad7cc68bce
gshoesshe-tech <gshoesshe@gmail.com>
2026-08-01 02:44:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Create a separate Supabase project for this client, then paste its values below.
 // Supabase Dashboard → Project Settings → API
 
-window.__SUPABASE_URL__ = "";
-window.__SUPABASE_ANON_KEY__ = "";
+window.__SUPABASE_URL__ = "https://votyujnrtezicuiejitn.supabase.co/";
+window.__SUPABASE_ANON_KEY__ = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdHl1am5ydGV6aWN1aWVqaXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MTU4ODcsImV4cCI6MjEwMTA5MTg4N30.0WNxal5FoHghIvjRf73LVFMPIwoe_jXoFYRtwn8NmH8";
